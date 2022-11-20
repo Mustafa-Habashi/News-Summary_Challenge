db25d1f8904cb547ee75810c2341b4474eb59881
Mustafa-Habashi <mustafahabashi10@gmail.com>
2022-11-20 19:10:21 +0000

    added summary to each article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../css/components.css'
 
-function Article({ title, thumbnail }) {
+function Article({ title, thumbnail, summary }) {
+    const [isOpen, setIsOpen] = useState(false)
+
+
     return (
-        <div className='article'>
-            <img className='articleTumbnail' src={thumbnail}></img>
-            <h1 className='articleHeadline'>{title}</h1>
 
+        <div className="card" onClick={() => setIsOpen(!isOpen)}>
+            <img className='articleTumbnail' src={thumbnail} alt='Headlines' ></img>
+            <div className="container">
+                <h4 className='articleHeadline'> {title}</h4>
+                {isOpen && <h5> <b>Summary</b></h5>}
+                {isOpen && <p className='summary'>{summary}</p>}
 
+            </div>
         </div>
     )
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import Article from './Article'
+import '../css/components.css'
 
 function HomePage({ headlines }) {
+
+    console.log(headlines)
 
     headlines.map(headline => (<li key={headline.id} >{headline.fields.headline}</li>))
 
 
     return (
-        <div>
+        <div className='articles'>
             <h2>HomePage/headlines:</h2>
             {/* <ul>{headlineListItems}</ul> */}
-            {headlines.map(headline => (<Article key={headline.id} thumbnail={headline.fields.thumbnail} title={headline.fields.headline}> </Article>))}
+            {headlines.map(headline => (
+                <Article key={headline.id} thumbnail={headline.fields.thumbnail} title={headline.fields.headline} summary={headline.fields.bodyText}> </Article>))}
 
 
         </div>
